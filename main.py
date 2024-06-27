85639a0a14176587ef4c6de87cb7288b4d6e73cd
Issac Mathai <82129993+IssacMathai@users.noreply.github.com>
2024-06-27 16:48:57 +1200

    Created basic about and users page
    Did this to learn how to pass in parameters via url

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ from flask import Flask
 
 app = Flask('simply-weather')
 
-@app.route('/')
-def index():
-   return 'Hello World!'
+@app.route('/')  
+def render_main():  
+    return 'This is the main page'
+
+@app.route('/all')
+@app.route('/about')  
+def render_about():  
+    return 'Project Information'
+
+@app.route('/user/<username>/')
+def show_profile(username):
+    return "Username: " + username
 
 
 app.run()

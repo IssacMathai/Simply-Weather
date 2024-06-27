f29c0e4aecde3b28d8cc195227adc28ee8e9a175
Issac Mathai <82129993+IssacMathai@users.noreply.github.com>
2024-06-27 18:08:39 +1200

    created jsonify hello message

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
-from flask import Flask
+from flask import Flask, request, make_response, jsonify
 
 app = Flask('simply-weather')
 
-@app.route('/')  
-def render_main():  
-    return 'This is the main page'
+@app.route('/')
+def no_data():
+    response = jsonify({'message': 'Hello there!', 'info': 'Using jsonify...', 'status': 200})
+    return response
+
+@app.route('/data/get_error/')
+def return_error():
+    response = make_response('<p>Oops... Sounds like an error!</p>', 400)
+    return response
 
 @app.route('/all')
 @app.route('/about')  
 def render_about():  
-    return 'Project Information'
+    return 'This is a weather web app.'
 
 @app.route('/user/<username>/')
 def show_profile(username):
     return "Username: " + username
 
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        template = """
+        <form method='POST'>
+        <input type='text' placeholder='Username...'>
+        <input type='password' placeholder='Password...'>
+        <input type='submit' value='Auth'>
+        </form>
+        """
+        return template
+
+    elif request.method == 'POST':
+        return 'Wow! Great, you logged in!'
 
 app.run()

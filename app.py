b535f30814e16f7f14f83e1a79475bde88f506d8
Issac Mathai <82129993+IssacMathai@users.noreply.github.com>
2024-07-01 20:10:23 +1200

    Implemented OpenWeatherMapAPI Integration
    - Weather info is sourced live from OpenWeatherMapAPI

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, make_response, jsonify, render_template
+import requests
+import os
 
 app = Flask(__name__)
+
+WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
 
 dict_with_weather_info = {
     'Taupo': [20, 'Sunny'],
@@ -12,9 +16,27 @@ dict_with_weather_info = {
 def index():
     if request.method == 'POST':
         city_name = request.form.get('city_name')
-        dict_with_weather_info[city_name] = ['Unknown', 'Unknown']
+        update_specific_city_weather(city_name)
 
     return render_template('index.html', weather=dict_with_weather_info)
+
+def update_specific_city_weather(city_name):
+    # Get lat and lon
+    payload = {'q': city_name, 'appid': WEATHER_API_KEY}
+    r = requests.get('http://api.openweathermap.org/geo/1.0/direct', params=payload)
+    lat = r.json()[0]['lat']
+    lon = r.json()[0]['lat']
+
+    # Get weather
+    payload = {'lat': lat, 'lon': lon, 'units': 'metric', 'appid': WEATHER_API_KEY}
+    r = requests.get('https://api.openweathermap.org/data/2.5/weather', params=payload)
+    state = r.json()['weather'][0]['main']
+    temp = r.json()['main']['temp']
+    dict_with_weather_info[city_name] = [state, temp]
+
+def update_all_weather():
+    for city, info in dict_with_weather_info.items():
+        update_specific_city_weather(city)
 
 
 @app.route('/profile')
@@ -33,8 +55,8 @@ def return_error():
     return response
 
 @app.route('/all')
-@app.route('/about')  
-def render_about():  
+@app.route('/about')
+def render_about():
     return 'This is a weather web app.'
 
 @app.route('/user/<username>/')
@@ -57,4 +79,5 @@ def login():
         return 'Wow! Great, you logged in!'
 
 if __name__ == '__main__':
+   update_all_weather()
    app.run(debug=True)
